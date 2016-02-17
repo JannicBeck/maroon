49ac0d6d68f0581adf053907ff9a3e0b0570e44c
Jb300 <jannicbeck@gmail.com>
2016-02-17 16:57:06 +0100

    lite version

--- maroon.js
+++ maroon.js
@@ -1,12 +1,12 @@
 function maroonCalendar(options) {
 
     // MODEL ---------------------------------------------------------------------------------------
+    var title = options.title;
     var placeholder = options.placeholder;
     var template = options.template;
     var locale = options.locale || 'en';
     moment.locale(locale);
     var intervalMode = options.intervalMode || false;
-    var startOfWeek = options.startOfWeek || 0;
     var timespan = options.timespan || [currentDate.year(), currentDate.clone().add(5, 'year')];
 
     var ROWS = 6;
@@ -14,14 +14,10 @@ function maroonCalendar(options) {
     var months = moment.months();
     var weekdays = moment.weekdays();
     var weekdaysMin = moment.weekdaysMin();
-    formatWeekdays();
     var currentDate = new moment();
     var today = new moment();
     var years = closedInterval(timespan[0], timespan[1]);
     var content = generateContent();
-    var startDate;
-    var interval;
-    var endDate;
 
     // inserts the view into the html using handlebars templating engine
     function render() {
@@ -35,34 +31,14 @@ function maroonCalendar(options) {
 
     // attention: under heavy construction, please put your helmet on for your own safety
     function generateView() {
-        var title = options.title || currentDate.format('dddd Do MMMM YYYY');
-
         var currentYear = currentDate.year();
         var currentMonth = months[currentDate.month()];
-
-        var viewWeekdays = generateViewWeekdays(weekdays);
-        var viewWeekdaysMin = generateViewWeekdays(weekdaysMin);
-        function generateViewWeekdays(weekdays) {
-            var viewWeekdays = weekdays.map(function(weekday, idx) {
-                var $weekday = $('<span></span>');
-                $weekday.html(weekday);
-                $weekday.addClass('maroonWeekday');
-                if (getWeekday(currentDate) === idx) {
-                    $weekday.addClass('primary');
-                }
-                return $weekday.prop('outerHTML');
-            });
-            return viewWeekdays;
-        }
 
         var viewMonths = months.map(function(month, idx) {
             var timeElement = $('<time></time>');
             timeElement.attr('dateTime', currentDate.format('YYYY-MM'));
             timeElement.html(month);
             timeElement.addClass('maroonInnerMonth');
-            if (currentDate.month() === idx) {
-                timeElement.addClass('primary');
-            }
             return timeElement.prop('outerHTML');
         });
 
@@ -71,9 +47,6 @@ function maroonCalendar(options) {
             timeElement.attr('dateTime', currentDate.format('YYYY'));
             timeElement.html(year);
             timeElement.addClass('maroonInnerYear');
-            if (years.indexOf(currentDate.year()) === idx) {
-                timeElement.addClass('primary');
-            }
             return timeElement.prop('outerHTML');
         });
 
@@ -91,34 +64,15 @@ function maroonCalendar(options) {
             if (compareDates(date, currentDate)) {
                 timeElement.addClass('current');
             }
-            if (intervalMode) {
-                if (startDate) {
-                    if (compareDates(date, startDate)) {
-                        timeElement.addClass('start');
-                    }
-                }
-                if (endDate) {
-                    if (compareDates(date, endDate)) {
-                        timeElement.addClass('end');
-                    }
-                }
-                if (interval) {
-                    interval.forEach(function(intervalDate) {
-                        if (compareDates(date, intervalDate)) {
-                            timeElement.addClass('interval');
-                        }
-                    });
-                }
-            }
+
             return timeElement.prop('outerHTML');
         });
 
         viewContent = toMatrix(viewContent, ROWS, COLS);
 
         return { years: viewYears, months: viewMonths,
-                weekdaysMin: viewWeekdaysMin, weekdays: viewWeekdays, currentDate,
-                currentYear, currentMonth, content: viewContent, title,
-                startDate: formatDate(startDate), endDate: formatDate(endDate) };
+                weekdays, weekdaysMin, currentDate,
+                currentYear, currentMonth, content: viewContent, title };
     }
     // ---------------------------------------------------------------------------------------------
 
@@ -136,10 +90,6 @@ function maroonCalendar(options) {
         styleParent('.maroonInnerYear', 'maroonYear');
         styleParent('.maroonInnerMonth', 'maroonMonth');
         styleParent('.maroonInnerDate', 'maroonDate');
-
-        var $weekdayElements = placeholder.find('.maroonWeekday');
-        var $currentWeekday = $weekdayElements.eq(getWeekday(currentDate));
-        $currentWeekday.parent().addClass('primary');
 
         function styleParent(childClass, cssClass) {
             var $child = placeholder.find(childClass);
@@ -233,28 +183,6 @@ function maroonCalendar(options) {
         return dateInterval;
     }
 
-    // format weekdays according to startOfWeek parameter
-    function formatWeekdays() {
-        var i = 0;
-        while (i < startOfWeek) {
-            weekdays.push(weekdays.shift());
-            weekdaysMin.push(weekdaysMin.shift());
-            i++;
-        }
-    }
-
-    // returns the day of the week according to startOfWeek
-    function getWeekday(date) {
-        var dayList = closedInterval(0, 6);
-        // reorder dayList according to startOfWeek
-        var i = 0;
-        while (i < startOfWeek) {
-            dayList.unshift(dayList.pop());
-            i++;
-        }
-        return dayList[date.day()];
-    }
-
     function compareDates(date, otherDate) {
         if( date.isSame(otherDate, 'day') &&
             date.isSame(otherDate, 'month') &&
@@ -269,10 +197,9 @@ function maroonCalendar(options) {
     // generates a 6*7 array with date objects as elements
     function generateContent() {
         var date = currentDate.clone();
-        // get start of month according to startOfWeek
         date.date(1);
         // 0 means start week on sunday, 1 monday ...
-        var startOfMonth = getWeekday(date);
+        var startOfMonth = date.weekday();
         var startOfContent = -startOfMonth + 1;
         date.date(startOfContent);
         var cellNumber = ROWS * COLS;
@@ -289,55 +216,6 @@ function maroonCalendar(options) {
             result[i] = a.splice(0, n);
         }
         return result;
-    }
-
-    function formatDate(date) {
-        if (date) {
-            return date.format('DD.MM.YYYY');
-        }
-    }
-
-    function activateIntervalMode() {
-        if (!startDate && !endDate) {
-            startDate = moment(currentDate);
-        } else if (startDate && !endDate) {
-            if (currentDate < startDate) {
-                startDate = moment(currentDate);
-            } else if (currentDate > startDate) {
-                endDate = moment(currentDate);
-                interval = closedDateInterval(startDate, endDate);
-            } else {
-                startDate = undefined;
-                interval = undefined;
-            }
-        } else if (!startDate && endDate) {
-            if (currentDate < endDate) {
-                startDate = moment(currentDate);
-                interval = closedDateInterval(startDate, endDate);
-            } else if (currentDate > endDate) {
-                endDate = moment(currentDate);
-            } else {
-                endDate = undefined;
-                interval = undefined;
-            }
-        } else if (startDate && endDate) {
-            if (currentDate < startDate) {
-                startDate = moment(currentDate);
-                interval = closedDateInterval(startDate, endDate);
-            } else if (currentDate > endDate) {
-                endDate = moment(currentDate);
-                interval = closedDateInterval(startDate, endDate);
-            } else if (currentDate > startDate && currentDate < endDate) {
-                endDate = moment(currentDate);
-                interval = closedDateInterval(startDate, endDate);
-            } else if (compareDates(currentDate, startDate)) {
-                startDate = undefined;
-                interval = undefined;
-            } else {
-                endDate = undefined;
-                interval = undefined;
-            }
-        }
     }
     // ---------------------------------------------------------------------------------------------
 
